7d6cee4069229137304753bd18bc46cdfba3f6e4
PikaGu <icpcpikagu@gmail.com>
2025-01-15 15:09:33 +0800

    env done

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ sys.path.append('./build')
 import cuda_demo
 
 # Generate random data
-a = np.random.rand(1000).astype(np.float32)
-b = np.random.rand(1000).astype(np.float32)
+a = np.random.rand(50).astype(np.float32)
+b = np.random.rand(50).astype(np.float32)
 
 # Call CUDA Kernel
-result = cuda_demo.vector_add(a, b)
+result = cuda_demo.add(a, b)
 
 # Verify result
 print("Result:", result)
--- src/pybind11_module.cpp
+++ src/pybind11_module.cpp
@@ -5,14 +5,17 @@
 
 namespace py = pybind11;
 
-void add(py::array_t<float> A, py::array_t<float> B, py::array_t<float> C) {
-    auto bufA = A.request(), bufB = B.request(), bufC = C.request(true);
-    float *ptrA = static_cast<float *>(bufA.ptr);
-    float *ptrB = static_cast<float *>(bufB.ptr);
-    float *ptrC = static_cast<float *>(bufC.ptr);
+py::array_t<float> add(py::array_t<float> A, py::array_t<float> B) {
+    auto bufA = A.request(), bufB = B.request();
+    float* ptrA = static_cast<float *>(bufA.ptr);
+    float* ptrB = static_cast<float *>(bufB.ptr);
     int N = bufA.size;
 
+    py::array_t<float> result(N);
+    auto bufC = result.request(true);
+    float* ptrC = static_cast<float *>(bufC.ptr);
     launch_vector_add(ptrA, ptrB, ptrC, N);
+    return result;
 }
 
 PYBIND11_MODULE(cuda_demo, m) {
